7a62145628907ae1e4f99937e8dbe56ecd47d997
Fun Noober <71403430+FunNoober@users.noreply.github.com>
2022-04-03 19:18:17 -0400

    Removed Unnecessary Files

--- SierraSandstormGodotEdition/Weapons/WeaponStats.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-export var fire_rate : float
-export var mag_size : int
-export var reserve_ammo : int
-export var fire_range : float
-export var reload_time : float
-export var max_accuracy : float
